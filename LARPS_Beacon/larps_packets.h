4bdb25bb31856d1cd4c4e25fad9b66043123d2b1
spaceneedle <spaceneedle@gmail.com>
2019-02-25 20:31:10 -0500

    Add files via upload

---
+++ LARPS_Beacon/larps_packets.h
@@ -0,0 +1,21 @@
+// Packet field positions
+
+#define PKT_VERSION 0
+#define PKT_LEN 1
+#define PKT_FROM 2
+#define PKT_TO 14
+#define PKT_RELAY 26
+#define PKT_COUNTER 38
+#define PKT_TTL 39
+#define PKT_OPTIONS 40
+#define PKT_TYPE 41
+#define PKT_DATA 42
+
+// Packet Values
+
+#define L_VERSION 0x01
+
+// Beacon Types
+
+#define L_TYPE_BEACON 0x0C
+
